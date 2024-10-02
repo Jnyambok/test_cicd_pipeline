12e8e31cdf63260a788534a6ad986551b39decf5
Jnyambok <juliusnyambok14@gmail.com>
2024-10-02 21:09:55 +0300

    Initial commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,7 +6,7 @@
 
 
 resource "aws_instance" "server" {
-    ami = "ami-0e5482f75bed66741"
+    ami = "xx"
     instance_type = "m5.large"   #free tier limits
     subnet_id = var.sn
     security_groups = [var.sg]
